33da5c3b73b0191242a88d8bdbffba0decb75e7a
Haakon Hasli <Haakon.Hasli@microsoft.com>
2019-11-15 12:02:36 +0000

    first version

---
+++ Program.cs
@@ -0,0 +1,23 @@
+using System;
+
+namespace hello
+{
+    class Program
+    {
+        static int number;
+        static string message = "Hei";
+
+        static void Main(string[] args)
+        {
+            Random eple = new Random();
+            number = eple.Next(32, 87);
+            string numster = number.ToString();
+            string finalmessage = string.Format("{0} {1}", message, numster);
+
+
+
+            Console.WriteLine($"{message} {numster}");
+
+        }
+    }
+}
